fd37ee31086cf7368dd48bd6f22b6a9a74d9180b
Chuck Thompson <chuckthompson27@me.com>
2018-12-11 16:59:52 -0800

    Refactored logic with case statement. More testing.

--- castles_and_creatures_test_spec.rb
+++ castles_and_creatures_test_spec.rb
@@ -1,15 +1,59 @@
 require 'rspec'
 require 'rspec/core'
-require_relative 'game'
+require 'json'
+require_relative 'gamestate'
+require_relative 'castle'
+require_relative 'player'
+require_relative 'room'
 
-
-describe Player do
+describe GameState do
   before :each do
     @player = Player.new
+    @castle = Castle.new("Hogwarts")
+    @room = Room.new("Closet", "Ogre", 95, "Golden Snitch", 100)
+
+    file = File.read('game-layout.json')
+    @castle_data = JSON.parse(file)
+    @castles = []
+    @castle_data.each_with_index do |castle, index|
+      @castles << Castle.new(@castle_data[index]["name"])
+    end
+
+    @castle_data.each_with_index do |castle, index|
+      @castle_data[index]["rooms"].each do |room|
+        @castles[index].rooms << Room.new(room["name"],
+                                          room["monster"]["name"],
+                                          room["monster"]["win_chance"],
+                                          room["treasure"]["type"],
+                                          room["treasure"]["points"])
+
+      end
+    end
   end
 
   it "initializes the player with lives and treasure" do
     expect(@player.lives).to eq(9)
     expect(@player.treasure).to eq(0)
+  end
+
+  it "initializes a castle with proper attributes" do
+    expect(@castle).to have_attributes(:name => "Hogwarts")
+    expect(@castle).to have_attributes(:rooms => [])
+  end
+
+  it "initializes a room with proper attributes" do
+    expect(@room).to have_attributes(:name => "Closet")
+    expect(@room).to have_attributes(:monster => "Ogre")
+    expect(@room).to have_attributes(:win_chance => 95)
+    expect(@room).to have_attributes(:treasure => "Golden Snitch")
+    expect(@room).to have_attributes(:points => 100)
+  end
+
+  it "initializes the game with the appropriate amount of castles" do
+    expect(@castle_data.count).to eq(5)
+  end
+
+  it "initializes the game with the appropriate amount of rooms" do
+    expect(@castles[0].rooms.count).to eq(3)
   end
 end
--- gamestate.rb
+++ gamestate.rb
@@ -14,6 +14,7 @@ class GameState
     @castle_order = 0
     @room_order = 0
     @player = Player.new
+    @move_list = %w(Fight Bluff Treasure Lives)
     @castle_data.each_with_index do |castle, index|
       @castles << Castle.new(@castle_data[index]["name"])
     end
@@ -30,7 +31,7 @@ class GameState
   end
 
   def play
-    unless game_over
+    unless game_over?
       castle_progression
       room_progression
       monster_encounter
@@ -58,59 +59,82 @@ class GameState
   end
 
   def player_move
-    fighting_chance = @castles[@castle_order].rooms[@room_order].win_chance
-    bluffing_chance = 30
-    current_treasure = @castles[@castle_order].rooms[@room_order].treasure
-    treasure_points = @castles[@castle_order].rooms[@room_order].points
+    @fighting_chance = @castles[@castle_order].rooms[@room_order].win_chance
+    @bluffing_chance = 30
+    @current_treasure = @castles[@castle_order].rooms[@room_order].treasure
+    @treasure_points = @castles[@castle_order].rooms[@room_order].points
     puts "*" * 25
     puts "What would you like to do?"
-    puts "Fight | Bluff"
+    puts @move_list
     puts "*" * 25
     @selected_move = gets.chomp
-    if @selected_move == "Fight"
-      if rand(100) < fighting_chance
+
+    case @selected_move.downcase
+    when "fight"
+      if fight_successful?
         puts "You successfully defeated the #{@current_monster}!"
-        puts "You find a #{current_treasure} clutched in a hand of the now lifeless #{@current_monster}."
-        puts "You quickly put it into your pouch. (+#{treasure_points} pts!)"
-        @player.treasure += treasure_points
-        if @current_room != @castles[@castle_order].rooms.last.name
-          @room_order += 1
-        elsif win_condition
-          puts "You have collected all the treasure!"
-          puts "Your score is #{@player.treasure} points!"
-        else
-          @castle_order += 1
-          @room_order = 0
-        end
+        puts "You find a #{@current_treasure} clutched in a hand of the now lifeless #{@current_monster}."
+        reset_move_list
+        progress_and_check_for_win
       else
+        @player.lives -= 1
         puts "The #{@current_monster} has defeated you."
         puts "You have #{@player.lives} lives left."
         puts "*" * 25
-        @player.lives -= 1
         player_move
       end
-
-    elsif @selected_move == "Bluff"
-      if rand(100) < bluffing_chance
-        puts "You successfully scare the #{@current_monster} and cause them to flee!"
-        puts "You find a #{current_treasure} on the floor where the #{@current_monster} was standing."
-        puts "You quickly put it into your pouch. (+#{treasure_points} pts!)"
-      else
-        puts "Your attempts to scare the #{@current_monster} have failed!"
-        puts "Looks like you will have to fight your way out!"
+    when "bluff"
+      if @move_list.include?("Bluff")
+        if bluff_successful?
+          puts "You successfully scare the #{@current_monster} and cause them to flee!"
+          puts "You find a #{@current_treasure} on the floor where the #{@current_monster} was standing."
+          progress_and_check_for_win
+        else
+          puts "Your attempts to scare the #{@current_monster} have failed!"
+          puts "Looks like you will have to fight your way out!"
+          @move_list.delete("Bluff")
         end
+      else
+       puts "You tried that already!"
+      end
+    when "treasure"
+      puts "You currently have #{@treasure_points} points."
+    when "lives"
+      puts "You currently have #{@player.lives} left."
     else
-      "ERROR: Please select a valid action!"
+      puts "ERROR: Please select a valid action!"
       player_move
     end
   end
 
-  def game_over
-    @player.lives < 0
+  def game_over?
+    @player.lives == 0
   end
 
   def win_condition
     @castle_order == @castles.count && @room_order == @castles[@castle_order].rooms.count
+  end
+  
+  def fight_successful?
+    rand(100) < @fighting_chance
+  end
+
+  def bluff_successful?
+    rand(100) < @bluffing_chance
+  end
+
+  def progress_and_check_for_win
+    puts "You quickly put it into your pouch. (+#{@treasure_points} pts!)"
+    @player.treasure += @treasure_points
+    if @current_room != @castles[@castle_order].rooms.last.name
+      @room_order += 1
+    elsif win_condition
+      puts "You have collected all the treasure!"
+      puts "Your score is #{@player.treasure} points!"
+    else
+      @castle_order += 1
+      @room_order = 0
+    end
   end
 
   def reset
@@ -123,5 +147,9 @@ class GameState
     @player.treasure = 0
     @castle_order = 0
     @room_order = 0
+  end
+
+  def reset_move_list
+    @move_list = %w(Fight Bluff Treasure Lives)
   end
 end
