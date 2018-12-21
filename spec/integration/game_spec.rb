51f0c429ed7dca9c92f9126c4ff8486ce2632b62
Chuck Thompson <chuckthompson27@me.com>
2018-12-21 12:58:21 -0800

    features and testing

--- spec/integration/game_spec.rb
+++ spec/integration/game_spec.rb
@@ -5,23 +5,29 @@ require_relative '../../lib/gamestate.rb'
 describe Gamestate do
   let(:gamestate) { Gamestate.new(@castle_data) }
 
-  before do
-    file = File.read('./spec/integration/config/game-layout-win.json')
-    @castle_data = JSON.parse(file)
+  context "winning conditions" do
+    before do
+      file = File.read('./spec/integration/config/game-layout-win.json')
+      @castle_data = JSON.parse(file)
+    end
+
+    it "validates the player can win a game" do
+      allow(STDIN).to receive(:gets).and_return("fight")
+      expect(gamestate).to receive(:win)
+      gamestate.play
+    end
   end
 
-  it "validates the player can win a game" do
-    allow(STDIN).to receive(:gets).and_return("fight")
-    gamestate.play
-  end
+  context "losing conditions" do
+    before do
+      file = File.read('./spec/integration/config/game-layout-lose.json')
+      @castle_data = JSON.parse(file)
+    end
 
-  before do
-    file = File.read('./spec/integration/config/game-layout-lose.json')
-    @castle_data = JSON.parse(file)
-  end
-
-  it "validates the player can lose a game" do
-    allow(STDIN).to receive(:gets).and_return("fight")
-    gamestate.play
+    it "validates the player can lose a game" do
+      allow(STDIN).to receive(:gets).and_return("fight")
+      expect(gamestate).to receive(:ask_player_to_try_again)
+      gamestate.play
+    end
   end
 end
