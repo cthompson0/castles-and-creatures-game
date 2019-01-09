3ee9f02d6141b447e85f960174f2c4f33071b016
Chuck Thompson <chuckthompson27@me.com>
2019-01-09 09:12:10 -0800

    Refactored integration test

--- spec/integration/game_spec.rb
+++ spec/integration/game_spec.rb
@@ -1,30 +1,17 @@
-require_relative '../../lib/gamestate.rb'
+require_relative '../../lib/game_interface.rb'
 
-describe Gamestate do
-  let(:game) { Gamestate.new(@castle_data) }
-
-  before do
-    @castle_data = JSON.parse(file)
-  end
-
+describe GameInterface do
   context "winning conditions" do
-    let(:file) { File.read('./spec/integration/config/game-layout-win.json') }
-
     it "validates the player can win a game" do
-      allow(STDIN).to receive(:gets).and_return("fight")
-      expect(game).to receive(:win)
-      expect(game).to receive(:ask_player_to_try_again)
-      game.play
+      allow(STDIN).to receive(:gets).and_return("./spec/integration/config/game-layout-win.json", "fight")
+      expect{ GameInterface.play }.to output(/You have successfully collected all the treasure/).to_stdout
     end
   end
 
   context "losing conditions" do
-    let(:file) { File.read('./spec/integration/config/game-layout-lose.json') }
-
     it "validates the player can lose a game" do
-      allow(STDIN).to receive(:gets).and_return("fight")
-      expect(game).to receive(:ask_player_to_try_again)
-      game.play
+      allow(STDIN).to receive(:gets).and_return("./spec/integration/config/game-layout-lose.json", "fight")
+      expect{ GameInterface.play }.to output(/You currently have 0 lives left./).to_stdout
     end
   end
 end
